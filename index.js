225ba2220a42ea4ba634de64a6d82556bc48e608
KannonH2 <manaxtatu@gmail.com>
2022-05-09 17:50:49 -0300

    clean

--- index.js
+++ index.js
@@ -3,7 +3,3 @@ const server = require('./src/app.js');
   server.listen(process.env.PORT, () => {
     console.log('listening at 3001'); // eslint-disable-line no-console
   });
-
-  // server.listen(3001, () => {
-  //   console.log('listening at 3001'); // eslint-disable-line no-console
-  // });
